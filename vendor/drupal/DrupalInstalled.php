ede1b85c206b6ac8a9383a74de66d238eb0a2089
Miguel <overnotpowered@gmail.com>
2026-03-04 13:08:42 +0100

    requisito funcional 4 finalizado

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '31d15c5a48fc91f2';
+  public const string VERSIONS_HASH = 'a04572d2ce50125e';
 
 }
