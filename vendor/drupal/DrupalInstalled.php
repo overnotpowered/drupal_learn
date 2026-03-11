0ecfe35730a2bce5b9bd9d986183981e03bb0fea
Miguel <overnotpowered@gmail.com>
2026-03-11 16:01:15 +0100

    agregados resto de archivos faltantes

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'a7e9c46759ad2543';
+  public const string VERSIONS_HASH = 'b542f541b5f4bea4';
 
 }
